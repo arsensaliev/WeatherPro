a38d4646259cadf56ed01848cefd12439679c494
Arsen Saliev <arsensaliev03@gmail.com>
2020-12-13 13:00:16 +0600

    feat: add textviews in forecast row item

--- app/src/main/java/com/weatherpro/fragments/forecast/RecyclerViewHolder.java
+++ app/src/main/java/com/weatherpro/fragments/forecast/RecyclerViewHolder.java
@@ -1,11 +1,16 @@
 package com.weatherpro.fragments.forecast;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    private TextView temperatureView;
+    private TextView dateView;
+    private TextView descriptionView;
+
     public RecyclerViewHolder(@NonNull View itemView) {
         super(itemView);
     }
