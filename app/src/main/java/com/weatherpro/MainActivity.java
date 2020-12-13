1b319fbdbb7ab23926703e0038ced0ce69f5f590
Arsen Saliev <arsensaliev03@gmail.com>
2020-12-13 12:00:13 +0600

    feat: add forecast models

--- app/src/main/java/com/weatherpro/MainActivity.java
+++ app/src/main/java/com/weatherpro/MainActivity.java
@@ -1,9 +1,12 @@
 package com.weatherpro;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -21,106 +24,119 @@ import com.weatherpro.fragments.MainFragment;
 import com.weatherpro.fragments.developer.DeveloperFragment;
 import com.weatherpro.fragments.location.LocationFragment;
 
-    public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-        private DrawerLayout drawerLayout;
-        private ActionBarDrawerToggle actionBarDrawerToggle;
-        private Toolbar toolbar;
-        private NavigationView navigationView;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private TextView drawerHeaderTextView;
+    // Fragments
+    private FragmentTransaction fragmentTransaction;
+    private Fragment fragment;
 
-        // Fragments
-        private FragmentTransaction fragmentTransaction;
-        private Fragment fragment;
+    private String cityName;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main_activity);
-            toolbar = findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-            drawerLayout = findViewById(R.id.drawer);
-            navigationView = findViewById(R.id.navigationView);
-            actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
-            drawerLayout.addDrawerListener(actionBarDrawerToggle);
-            actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
-            actionBarDrawerToggle.syncState();
-            navigationView.setNavigationItemSelectedListener(this);
-            pushFragments(new MainFragment(), null);
-        }
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        private OnQueryTextListener queryTextListener;
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        drawerHeaderTextView = navigationView.getHeaderView(0).findViewById(R.id.drawer_header_text);
 
-        public void setQueryTextListener(OnQueryTextListener queryTextListener) {
-            this.queryTextListener = queryTextListener;
-        }
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
-        public interface OnQueryTextListener {
-            boolean onQueryTextSubmit(String query);
-
-            boolean onQueryTextChange(String newText);
-        }
-
-        public void pushFragments(Fragment fragment, Bundle bundle) {
-            if (bundle != null) {
-                fragment.setArguments(bundle);
-            }
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out);
-            fragmentTransaction.replace(R.id.container_fragment, fragment);
-            fragmentTransaction.commit();
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-            MenuItem item = menu.findItem(R.id.app_bar_search);
-            SearchView searchView = (SearchView) item.getActionView();
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    if (queryTextListener != null) {
-                        queryTextListener.onQueryTextSubmit(query);
-                    }
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    if (queryTextListener != null) {
-                        queryTextListener.onQueryTextChange(newText);
-                    }
-                    return false;
-                }
-            });
-            return super.onCreateOptionsMenu(menu);
-        }
-
-
-        @SuppressLint("NonConstantResourceId")
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-            switch (item.getItemId()) {
-                case R.id.home:
-                    fragment = new MainFragment();
-                    pushFragments(fragment, null);
-                    toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(false);
-                    break;
-                case R.id.developer:
-                    fragment = new DeveloperFragment();
-                    pushFragments(fragment, null);
-                    toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(false);
-                    break;
-                case R.id.location:
-                    fragment = new LocationFragment();
-                    pushFragments(fragment, null);
-                    toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(true);
-                default:
-                    pushFragments(fragment, null);
-            }
-            return true;
-        }
-
+        loadCityName();
+        pushFragments(new MainFragment(), null);
     }
+
+    private OnQueryTextListener queryTextListener;
+
+    public void setQueryTextListener(OnQueryTextListener queryTextListener) {
+        this.queryTextListener = queryTextListener;
+    }
+
+    public interface OnQueryTextListener {
+        boolean onQueryTextSubmit(String query);
+
+        boolean onQueryTextChange(String newText);
+    }
+
+    public void pushFragments(Fragment fragment, Bundle bundle) {
+        if (bundle != null) {
+            fragment.setArguments(bundle);
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out);
+        fragmentTransaction.replace(R.id.container_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        MenuItem item = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (queryTextListener != null) {
+                    queryTextListener.onQueryTextSubmit(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (queryTextListener != null) {
+                    queryTextListener.onQueryTextChange(newText);
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        switch (item.getItemId()) {
+            case R.id.home:
+                fragment = new MainFragment();
+                pushFragments(fragment, null);
+                toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(false);
+                break;
+            case R.id.developer:
+                fragment = new DeveloperFragment();
+                pushFragments(fragment, null);
+                toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(false);
+                break;
+            case R.id.location:
+                fragment = new LocationFragment();
+                pushFragments(fragment, null);
+                toolbar.getMenu().findItem(R.id.app_bar_search).setVisible(true);
+                break;
+            default:
+                pushFragments(new MainFragment(), null);
+        }
+        return true;
+    }
+
+    public void loadCityName() {
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.MAIN_SHARED_NAME, Context.MODE_PRIVATE);
+        cityName = sharedPreferences.getString(Constants.SHARED_COUNTRY_NAME, Constants.SHARED_COUNTRY_NAME_DEFAULT);
+        drawerHeaderTextView.setText(cityName);
+    }
+}
--- app/src/main/java/com/weatherpro/fragments/location/LocationFragment.java
+++ app/src/main/java/com/weatherpro/fragments/location/LocationFragment.java
@@ -32,100 +32,101 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
-    public class LocationFragment extends Fragment {
-        private static final String TAG = "LOCATION_FRAGMENT";
+public class LocationFragment extends Fragment {
+    private static final String TAG = "LOCATION_FRAGMENT";
 
-        private MainActivity mainActivity;
-        private RecyclerAdapter recyclerAdapter;
-        private List<CityModel> cityModelList;
-        private SearchView searchView;
-        private SharedPreferences.Editor editor;
+    private MainActivity mainActivity;
+    private RecyclerAdapter recyclerAdapter;
+    private List<CityModel> cityModelList;
+    private SearchView searchView;
+    private SharedPreferences.Editor editor;
 
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            mainActivity = (MainActivity) getActivity();
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_location, container, false);
-            init(view);
-            return view;
-        }
-
-        @SuppressLint("CommitPrefEdits")
-        private void init(View view) {
-            SharedPreferences sharedPreferences = getContext().getSharedPreferences(Constants.MAIN_SHARED_NAME, Context.MODE_PRIVATE);
-            editor = sharedPreferences.edit();
-            initRecyclerView(view);
-            initSearchListener();
-        }
-
-        private void initSearchListener() {
-            mainActivity.setQueryTextListener(new MainActivity.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    recyclerAdapter.getFilter().filter(newText);
-                    return false;
-                }
-            });
-        }
-
-        private void initRecyclerView(View view) {
-            RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-
-            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(view.getContext(), LinearLayoutManager.VERTICAL);
-            recyclerView.addItemDecoration(dividerItemDecoration);
-
-            Gson gson = new Gson();
-            String cities = getCities();
-            cityModelList = gson.fromJson(cities, CitiesModel.class).getList();
-
-            recyclerAdapter = new RecyclerAdapter(cityModelList);
-            recyclerAdapter.SetOnItemClickListener((lat, lon, cityName) -> {
-                editor.putBoolean(Constants.SHARED_IS_COUNTRY_EMPTY, false);
-                editor.putString(Constants.SHARED_COUNTRY_NAME, cityName);
-                editor.putInt(Constants.SHARED_COUNTRY_LAT, lat);
-                editor.putInt(Constants.SHARED_COUNTRY_LON, lon);
-                editor.apply();
-                mainActivity.pushFragments(new MainFragment(), null);
-            });
-            recyclerView.setAdapter(recyclerAdapter);
-        }
-
-        private String getCities() {
-            StringBuilder buf = new StringBuilder();
-            InputStream json = null;
-            try {
-                json = getContext().getAssets().open(Constants.FILE_CITIES);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            BufferedReader in = new BufferedReader(new InputStreamReader(json, StandardCharsets.UTF_8));
-            String str = null;
-
-            while (true) {
-                try {
-                    if ((str = in.readLine()) == null) break;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                buf.append(str);
-            }
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return buf.toString();
-        }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainActivity = (MainActivity) getActivity();
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        init(view);
+        return view;
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private void init(View view) {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(Constants.MAIN_SHARED_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        initRecyclerView(view);
+        initSearchListener();
+    }
+
+    private void initSearchListener() {
+        mainActivity.setQueryTextListener(new MainActivity.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+    private void initRecyclerView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(view.getContext(), LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        Gson gson = new Gson();
+        String cities = getCities();
+        cityModelList = gson.fromJson(cities, CitiesModel.class).getList();
+
+        recyclerAdapter = new RecyclerAdapter(cityModelList);
+        recyclerAdapter.SetOnItemClickListener((lat, lon, cityName) -> {
+            editor.putBoolean(Constants.SHARED_IS_COUNTRY_EMPTY, false);
+            editor.putString(Constants.SHARED_COUNTRY_NAME, cityName);
+            editor.putInt(Constants.SHARED_COUNTRY_LAT, lat);
+            editor.putInt(Constants.SHARED_COUNTRY_LON, lon);
+            editor.apply();
+            mainActivity.pushFragments(new MainFragment(), null);
+            mainActivity.loadCityName();
+        });
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private String getCities() {
+        StringBuilder buf = new StringBuilder();
+        InputStream json = null;
+        try {
+            json = getContext().getAssets().open(Constants.FILE_CITIES);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(json, StandardCharsets.UTF_8));
+        String str = null;
+
+        while (true) {
+            try {
+                if ((str = in.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            buf.append(str);
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buf.toString();
+    }
+}
