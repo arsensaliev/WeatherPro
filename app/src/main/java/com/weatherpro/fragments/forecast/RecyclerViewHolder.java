3a6f17dcff255772f2fefb632b1364b7aa69d5d3
Arsen Saliev <arsensaliev03@gmail.com>
2020-12-13 12:29:16 +0600

    refactor: rename file row_item on fragment_location_row_item

---
+++ app/src/main/java/com/weatherpro/fragments/forecast/RecyclerViewHolder.java
@@ -0,0 +1,12 @@
+package com.weatherpro.fragments.forecast;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
